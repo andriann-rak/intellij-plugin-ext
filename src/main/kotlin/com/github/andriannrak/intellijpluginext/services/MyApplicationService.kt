11ecd5ff4100349d4580936bfd60d8079f0e2b2f
GitHub Action <action@github.com>
2022-09-01 20:23:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/andriannrak/intellijpluginext/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/andriannrak/intellijpluginext/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.andriannrak.intellijpluginext.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andriannrak.intellijpluginext.MyBundle
 
 class MyApplicationService {
 
